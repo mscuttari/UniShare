9e26bf07cb4c0c3ce5777857b229939e4cbe0b8c
Scuttari Michele <michele.scuttari@outlook.com>
2018-07-13 19:18:47 +0200

    Switched to Gradle

---
+++ Java/src/main/java/unishare/controllers/MainController.java
@@ -0,0 +1,15 @@
+package unishare.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController extends AbstractController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
