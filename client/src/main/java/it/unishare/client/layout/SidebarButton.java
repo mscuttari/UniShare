a1d4477fbe9d7dc97c774ee67a85ebd778b7af2b
Michele Scuttari <mscuttari@users.noreply.github.com>
2018-08-06 09:04:52 +0200

    Left menu entries

---
+++ client/src/main/java/it/unishare/client/layout/SidebarButton.java
@@ -0,0 +1,38 @@
+package it.unishare.client.layout;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.beans.NamedArg;
+import javafx.scene.control.ToggleButton;
+
+public class SidebarButton extends ToggleButton {
+
+    /**
+     * Constructor
+     */
+    public SidebarButton(@NamedArg(value = "icon") String iconName) {
+        setMaxWidth(Double.MAX_VALUE);  // Full width
+        getStyleClass().add("menus");   // Menu button style
+        setMnemonicParsing(false);      // Disable mnemonic parsing
+        setIcon(iconName);              // Set icon
+    }
+
+
+    /**
+     * Set icon name
+     *
+     * @param   iconName    Font Awesome icon name
+     */
+    public void setIcon(String iconName) {
+        if (iconName != null) {
+            try {
+                FontAwesomeIcon icon = FontAwesomeIcon.valueOf(iconName.toUpperCase());
+                FontAwesomeIconView iconView = new FontAwesomeIconView(icon);
+                setGraphic(iconView);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
